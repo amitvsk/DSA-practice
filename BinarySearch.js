003f36ff004ea27039e956aaab7ea08d744e1a72
Amit Vishwakarma <amitbabhanchak@gmail.com>
2023-05-19 08:34:43 +0530

    Update BinarySearch.js

--- BinarySearch.js
+++ BinarySearch.js
@@ -11,3 +11,13 @@ binarySort = (arr,start,end,num)=>{
        return false 
 }
 console.log(binarySort([1,2,3,4,5,6],0,6,3))
+
+function funAbc(arr,n){
+    let x;
+    if(n==1) return console.log("a",arr[0]);
+    else x= funAbc(arr,n-1);
+    if(x> arr[n-1]) return console.log("c",x);
+    else return console.log("d",arr[n-1]);
+}
+
+funAbc([12,10,30,50,100],5)
